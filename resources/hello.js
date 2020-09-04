c727cbce2e6e786f560b06841d0830681c8b144d
Matt Levine <matt.levine@blueriver.com>
2020-09-04 11:20:44 -0700

    Initial commit

---
+++ resources/hello.js
@@ -0,0 +1,43 @@
+/* 
+This code uses callbacks to handle asynchronous function responses.
+It currently demonstrates using an async-await pattern. 
+AWS supports both the async-await and promises patterns.
+For more information, see the following: 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/calling-services-asynchronously.html
+https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html 
+*/
+exports.main = async function(event, context) {
+  try {
+    var method = event.httpMethod;
+
+    if (method === "GET") {
+      if (event.path === "/") {
+     
+        var body = {
+          message: "Hello there!"
+        };
+        return {
+          statusCode: 200,
+          headers: {},
+          body: JSON.stringify(body)
+        };
+      }
+    }
+
+    // We only accept GET for now
+    return {
+      statusCode: 400,
+      headers: {},
+      body: "We only accept GET /"
+    };
+  } catch(error) {
+    var body = error.stack || JSON.stringify(error, null, 2);
+    return {
+      statusCode: 400,
+        headers: {},
+        body: JSON.stringify(body)
+    }
+  }
+}
